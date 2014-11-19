05d4d6fc0fe9ed60a7fe329a6f76da90bf8ab114
hselin <hselin.chen@gmail.com>
2014-11-18 18:02:16 -0800

    Add doc comm controller

---
+++ Ola/DoctorCommTVC.h
@@ -0,0 +1,13 @@
+//
+//  DoctorCommTVC.h
+//  Ola
+//
+//  Created by Albert Chen on 11/18/14.
+//  Copyright (c) 2014 cs147. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DoctorCommTVC : UITableViewController
+
+@end
