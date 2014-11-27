5aed031d1461c8c188d68c8b786ab455ef5250e8
hselin <hselin.chen@gmail.com>
2014-11-26 16:58:04 -0800

    Implemented home screen

---
+++ Ola/HomeScreenVC.h
@@ -0,0 +1,13 @@
+//
+//  HomeScreenVC.h
+//  Ola
+//
+//  Created by Albert Chen on 11/26/14.
+//  Copyright (c) 2014 cs147. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeScreenVC : UIViewController
+
+@end
